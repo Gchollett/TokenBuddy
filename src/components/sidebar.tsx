f2ab91c9663cc74c67dceb440e94ed5e9a29487f
garrett <garrettchollett@yahoo.com>
2023-12-18 00:39:33 -0600

    Updated sidebar arrow

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,18 +3,23 @@ import CastleIcon from '@mui/icons-material/Castle';
 import CastleOutlinedIcon from '@mui/icons-material/CastleOutlined';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 import MeetingRoomoutlinedIcon from '@mui/icons-material/MeetingRoomOutlined';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight'
 import { useState } from 'react'
 
 export default function SideBar(){
     const [monarch,setMonarch] = useState(false)
     const [initiative,setInitiative] = useState(false)
+    const [open,setOpen] = useState(false)
     return(
         <div id="sidebar" className='absolute flex right-0 h-full rounded-l-md bg-neutral-900 side-bar closed'>
             <button 
-                className='self-center relative right-8 bg-neutral-900 rounded-l-md'
-                onClick={()=>console.log(document.getElementById("sidebar")?.classList.toggle("closed"))}
+                className='self-center relative right-8 bg-neutral-900 rounded-l-md hover:text-green-300'
+                onClick={()=>{
+                    document.getElementById("sidebar")?.classList.toggle("closed")
+                    setOpen(!open)
+                }}
             >
-                <KeyboardDoubleArrowLeftIcon className='hover:text-green-300'/>
+                {!open?<KeyboardDoubleArrowLeftIcon/>:<KeyboardDoubleArrowRightIcon/>}
             </button>
             <div className="relative left-11 flex top-0 h-fit w-fit cursor-pointer" onClick={()=>{setMonarch(!monarch)}}>
                 {monarch?<CastleIcon/>:<CastleOutlinedIcon/>}
