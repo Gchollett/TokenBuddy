e491356dfa0d8dc79974b10b84e2e07850352b8c
garrett <garrettchollett@yahoo.com>
2024-01-08 00:39:40 -0600

    Added relative position to wrapper div to fix mobile issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Token Buddy',
   description: 'An token manager for Magic the Gathering',
-  viewport: {width: "device-width", initialScale: 1, minimumScale:1, maximumScale:1},
+  viewport: {width: "device-width", initialScale: 1, minimumScale: 1, maximumScale: 1},
 }
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className='w-full overflow-x-hidden'>
       <body suppressHydrationWarning={true} className='w-full overflow-x-hidden'>
-        <div className='w-full overflow-x-hidden'> {/* This div is a wrapper to prevent vertical overflow on IOS devices */}
+        <div className='relative w-full overflow-x-hidden'> {/* This div is a wrapper to prevent vertical overflow on IOS devices */}
           {children}
         </div>
       </body>
